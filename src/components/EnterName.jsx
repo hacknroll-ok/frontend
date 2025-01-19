3d6d48f3a751f2fb95a2a6186a48b1469684dd6a
Yeo Wei Jie David <140330770+awork2103@users.noreply.github.com>
2025-01-19 11:47:21 +0800

    Fixed game name

--- src/components/EnterName.jsx
+++ src/components/EnterName.jsx
@@ -43,7 +43,7 @@ export default function EnterPage() {
                     color="blue"
                     className="mb-4 grid h-28 place-items-center"
                 >
-                    <Typography variant="h3" color="white"> Scibble Animals </Typography>
+                    <Typography variant="h3" color="white"> Scribble Animals </Typography>
                 </CardHeader>
                 <CardBody>
                     <Input label="Enter Your Name" size="lg" value={name} onChange={(e) => setName(e.target.value)} />
